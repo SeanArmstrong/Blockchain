761d04b5554530981c64017a0a8d7e787c3716b0
Sean Armstrong <5382335+SeanArmstrong@users.noreply.github.com>
2018-10-19 15:58:47 +0100

    Correctly set hash of previous block

--- CreateABlockChain/blockchain.py
+++ CreateABlockChain/blockchain.py
@@ -14,6 +14,7 @@ class Block:
     self.timestamp = str(datetime.datetime.now())
     self.proof = proof
     self.previous_hash = previous_hash
+  # End __init__
 
   def as_json(self):
     return {
@@ -22,6 +23,8 @@ class Block:
        'proof': self.proof,
        'previous_hash': self.previous_hash
     }
+  # End as_json
+# End Block
 
 
 class Blockchain:
@@ -29,14 +32,17 @@ class Blockchain:
   def __init__(self):
     self.chain = []
     self.create_block(proof = 1, previous_hash = '0')
+  # End __init__
 
   def create_block(self, proof, previous_hash):
     block = Block(len(self.chain) + 1, proof, previous_hash)
     self.chain.append(block)
     return block
+  # End create_block
 
   def get_last_block(self):
     return self.chain[-1]
+  # End get_last_block
 
   def proof_of_work(self, previous_proof):
     new_proof = 1
@@ -69,27 +75,30 @@ class Blockchain:
         
         previous_block = block
         block_index += 1
-    # end while
+    # End while
 
     return True
-  # end is_chain_valid
+  # End is_chain_valid
 
   def as_json(self):
     return {
       'chain': map(lambda block: block.as_json(), self.chain),
       'length': len(self.chain)
     }
+  # End as_json
 
   def hash(self, code):
     return hashlib.sha256(str(code).encode()).hexdigest()
+  # End hash
 
   def hash_block(self, block):
     encoded_block = json.dumps(block, sort_keys = True).encode()
     return hash(encoded_block)
+  # End hash_block
 
   def valid_hash(self, hash_code):
     return hash_code[:4] == '0000'
-
+  # End valid_hash
 # End Blockchain
 
 # Part 2 - Mining our Blockchain
@@ -104,18 +113,17 @@ blockchain = Blockchain()
 def mine_block():
   last_block = blockchain.get_last_block()
   proof = blockchain.proof_of_work(last_block.proof)
-  new_block = blockchain.create_block(proof, last_block.previous_hash)
+  previous_hash = blockchain.hash(last_block)
+  new_block = blockchain.create_block(proof, previous_hash)
   response = new_block.as_json()
   response.update({'messages': 'Congratulations, you just mined a block'})
 
   return jsonify(response), 200
+# End mine_block
 
 @app.route('/get_chain', methods = ['GET'])
 def get_chain():
-  response = {
-    'chain': map(lambda block: block.as_json(), blockchain.chain),
-    'length': len(blockchain.chain)
-  }
   return jsonify(blockchain.as_json(), 200)
+# End get_chain
 
 # app.run('0.0.0.0', 5000)
